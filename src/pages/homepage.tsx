dae8c7a17ae0ee4cb50ab8875f26a550c51e159e
AlexandruStefanGherhes <104617757+AlexandruStefanGherhes@users.noreply.github.com>
2024-05-09 21:52:31 +0300

    Update homepage.tsx

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -11,7 +11,7 @@ return (
 <div className="font-jost overflow-hidden md:flex md:flex-col 2xl:max-w-[60%] 2xl:mr-auto xl:ml-auto">
     <Nav/>
     <section className="bg-[#E7816B] mt-6 family-jost md:w-[93.5%] md:rounded-2xl md:self-center lg:mt-14"> 
-        <article className="hero flex flex-col text-center p-4 pt-16 pb-0  text-white items-center bg-[url('assets/contact/desktop/bg-pattern-hero-desktop.svg')] bg-left bg-cover bg-no-repeat lg:items-start lg:text-start lg:flex-row ">
+        <article className="hero flex flex-col text-center p-4 pt-16 pb-0  text-white items-center bg-[url(assets/contact/desktop/bg-pattern-hero-desktop.svg)] bg-left bg-cover bg-no-repeat lg:items-start lg:text-start lg:flex-row ">
             <div className='lg:ml-24 lg:mt-24 flex flex-col items-center lg:items-start'>
                 <h1 className="mb-4 text-3xl font-semibold md:w-4/5 md:text-5xl md:tracking-wide lg:w-full lg:leading-16">Award-winning custom designs and digital branding solutions</h1>
                 <p className="pb-10 w-[88%] whitespace-pre-line md:w-4/6 md:mt-6 lg:w-[90%] lg:text-xl ">
@@ -21,7 +21,7 @@ return (
                     <Link to='/about' className='w-full md:w-2/4'><Button text='Learn More'/></Link>
                 </div>
             </div>
-            <div className="bg-[url(assets/home/desktop/image-hero-phone.png)] bg-top w-[110%] h-[35rem] mt-[-5rem] bg-no-repeat lg:bg-[right_0rem_top_0rem] lg:h-[52rem] ">
+            <div className="bg-[url(./assets/home/desktop/image-hero-phone.png)] bg-top w-[110%] h-[35rem] mt-[-5rem] bg-no-repeat lg:bg-[right_0rem_top_0rem] lg:h-[52rem] ">
             </div>
         </article>
     </section>
